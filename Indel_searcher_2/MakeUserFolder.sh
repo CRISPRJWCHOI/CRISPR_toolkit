afc422f193539f870f422f617e578df2a5746496
CRISPRJWCHOI <abmrcbioinformatics@gmail.com>
2019-09-23 13:06:42 +0900

    fix

--- Indel_searcher_2/MakeUserFolder.sh
+++ Indel_searcher_2/MakeUserFolder.sh
@@ -7,7 +7,6 @@ project=JaeWoo_test_samples
 [ ! -d ./User ] && { `mkdir ./User`; }
 [ ! -d ./Output ] && { `mkdir ./Output`; }
 
-
 [ ! -d ./Input/${user} ] && { `mkdir ./Input/${user}`; }
 [ ! -d ./Input/${user}/FASTQ ] && { `mkdir ./Input/${user}/FASTQ`; }
 [ ! -d ./Input/${user}/FASTQ/${project} ] && { `mkdir ./Input/${user}/FASTQ/${project}`; }
