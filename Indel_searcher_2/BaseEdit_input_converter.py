6a174d5733eacda2d9410010ff138e924c4423c5
CRISPRJWCHOI <abmrcbioinformatics@gmail.com>
2019-09-26 14:04:10 +0900

    update

--- Indel_searcher_2/BaseEdit_input_converter.py
+++ Indel_searcher_2/BaseEdit_input_converter.py
@@ -1,6 +1,8 @@
 import os,sys
 from pdb import set_trace
 
+import multiprocessing as mp
+
 sys.path.insert(0, os.path.dirname(os.getcwd()))
 from Core.CoreSystem import Helper
 
@@ -30,89 +32,94 @@ Read
 YSKim_0525+01614_98_repeat1.txt
 """
 
-def Convert_Indelsearcher_output():
+def Convert_Indelsearcher_output(strSampleRefGroup):
 
-    with open('./User/{user}/{project}.txt'.format(user=strUser,project=strProject)) as SampleList:
-        for strSampleRefGroup in SampleList:
-            if strSampleRefGroup[0] == '#': continue
-            listSampleRefGroup = strSampleRefGroup.replace('\n', '').split('\t')
+    listSampleRefGroup = strSampleRefGroup.replace('\n', '').split('\t')
 
-            strSample = listSampleRefGroup[0]
-            strRef    = listSampleRefGroup[1]
+    strSample = listSampleRefGroup[0]
+    strRef    = listSampleRefGroup[1]
 
-            print('Processing: %s, %s' % (strSample, strRef))
+    print('Processing: %s, %s' % (strSample, strRef))
 
-            strBaseEditRefFolder   = '../Base_edit_2/Input/{user}/Reference/{project}/{ref}'.format(user=strUser,
-                                                                                                    project=strProject,
-                                                                                                    ref=strRef)
-            strBaseEditQueryFolder = '../Base_edit_2/Input/{user}/Query/{project}/{sample}'.format(user=strUser,
-                                                                                                   project=strProject,
-                                                                                                   sample=strSample)
+    strBaseEditRefFolder   = '../Base_edit_2/Input/{user}/Reference/{project}/{ref}'.format(user=strUser,
+                                                                                            project=strProject,
+                                                                                            ref=strRef)
+    strBaseEditQueryFolder = '../Base_edit_2/Input/{user}/Query/{project}/{sample}'.format(user=strUser,
+                                                                                           project=strProject,
+                                                                                           sample=strSample)
 
-            Helper.MakeFolderIfNot(strBaseEditRefFolder)
-            Helper.MakeFolderIfNot(strBaseEditQueryFolder)
+    Helper.MakeFolderIfNot(strBaseEditRefFolder)
+    Helper.MakeFolderIfNot(strBaseEditQueryFolder)
 
-            ## BaseEdit refer format : filename, barcode, reference
-            ReferenceFile_in_IndelSearcher = open('./Input/{user}/Reference/{project}/{ref}/Reference_sequence.txt'.format(user=strUser,
-                                                                                                                           project=strProject,
-                                                                                                                           ref=strRef))
-            BarcodeFile_in_IndelSearcher   = open('./Input/{user}/Reference/{project}/{ref}/Barcode.txt'.format(user=strUser,
-                                                                                                                project=strProject,
-                                                                                                                ref=strRef))
-            BarcodeFile_for_BaseEdit       = open('../Base_edit_2/Input/{user}/Reference/{project}/{ref}/Barcode.txt'.format(user=strUser,
-                                                                                                                             project=strProject,
-                                                                                                                             ref=strRef), 'w')
-            Reference_for_BaseEdit         = open('../Base_edit_2/Input/{user}/Reference/{project}/{ref}/Reference.txt'.format(user=strUser,
-                                                                                                                               ref=strRef,
-                                                                                                                               project=strProject), 'w') ## conversion target to barcode:refseq
+    ## BaseEdit refer format : filename, barcode, reference
+    ReferenceFile_in_IndelSearcher = open('./Input/{user}/Reference/{project}/{ref}/Reference_sequence.txt'.format(user=strUser,
+                                                                                                                   project=strProject,
+                                                                                                                   ref=strRef))
+    BarcodeFile_in_IndelSearcher   = open('./Input/{user}/Reference/{project}/{ref}/Barcode.txt'.format(user=strUser,
+                                                                                                        project=strProject,
+                                                                                                        ref=strRef))
+    BarcodeFile_for_BaseEdit       = open('../Base_edit_2/Input/{user}/Reference/{project}/{ref}/Barcode.txt'.format(user=strUser,
+                                                                                                                     project=strProject,
+                                                                                                                     ref=strRef), 'w')
+    Reference_for_BaseEdit         = open('../Base_edit_2/Input/{user}/Reference/{project}/{ref}/Reference.txt'.format(user=strUser,
+                                                                                                                       ref=strRef,
+                                                                                                                       project=strProject), 'w') ## conversion target to barcode:refseq
 
-            dictBarcodeSeq = {}
+    dictBarcodeSeq = {}
 
-            for strBarcodeIndelSearcher, strReferenceIndelSearcher in zip(BarcodeFile_in_IndelSearcher, ReferenceFile_in_IndelSearcher):
+    for strBarcodeIndelSearcher, strReferenceIndelSearcher in zip(BarcodeFile_in_IndelSearcher, ReferenceFile_in_IndelSearcher):
 
-                strBarcodeIndelSearcher   = strBarcodeIndelSearcher.replace('\n', '').strip()
-                strReferenceIndelSearcher = strReferenceIndelSearcher.replace('\n', '').strip()
+        strBarcodeIndelSearcher   = strBarcodeIndelSearcher.replace('\n', '').strip()
+        strReferenceIndelSearcher = strReferenceIndelSearcher.replace('\n', '').strip()
 
-                dictBarcodeSeq[strBarcodeIndelSearcher] = []
-                BarcodeFile_for_BaseEdit.write(strBarcodeIndelSearcher + ':' + strBarcodeIndelSearcher + '\n') ## first is filename, second is barcode. BaseEdit barcode format
-                Reference_for_BaseEdit.write(strBarcodeIndelSearcher + ':' + strReferenceIndelSearcher + '\n')
+        dictBarcodeSeq[strBarcodeIndelSearcher] = []
+        BarcodeFile_for_BaseEdit.write(strBarcodeIndelSearcher + ':' + strBarcodeIndelSearcher + '\n') ## first is filename, second is barcode. BaseEdit barcode format
+        Reference_for_BaseEdit.write(strBarcodeIndelSearcher + ':' + strReferenceIndelSearcher + '\n')
 
-            ReferenceFile_in_IndelSearcher.close()
-            BarcodeFile_in_IndelSearcher.close()
-            Reference_for_BaseEdit.close()
+    ReferenceFile_in_IndelSearcher.close()
+    BarcodeFile_in_IndelSearcher.close()
+    Reference_for_BaseEdit.close()
 
-            Total_result_file = open('./Output/{user}/{project}/{sample}/Tmp/{sample}_Classified_Indel_barcode.fastq'.format(user=strUser,
-                                                                                                                             project=strProject,
-                                                                                                                             sample=strSample))
+    Total_result_file = open('./Output/{user}/{project}/{sample}/Tmp/{sample}_Classified_Indel_barcode.fastq'.format(user=strUser,
+                                                                                                                     project=strProject,
+                                                                                                                     sample=strSample))
 
+    intCheckTotLine = 0
+    intOneLineMore  = 0
+
+    for i, strRow in enumerate(Total_result_file):  ## for query reads
+
+        if intOneLineMore == 1:
             intCheckTotLine = 0
             intOneLineMore  = 0
 
-            for i, strRow in enumerate(Total_result_file):  ## for query reads
+        if i % 4 == 0: ## Classified_Indel_barcode has all total sequence.
+            strBarcode = strRow.split('Barcode_')[1].split(':')[0]
+            intCheckTotLine = 1
 
-                if intOneLineMore == 1:
-                    intCheckTotLine = 0
-                    intOneLineMore  = 0
+        elif intCheckTotLine == 1:
+            dictBarcodeSeq[strBarcode].append(strRow)
+            intOneLineMore = 1
 
-                if i % 4 == 0: ## Classified_Indel_barcode has all total sequence.
-                    strBarcode = strRow.split('Barcode_')[1].split(':')[0]
-                    intCheckTotLine = 1
+    for strBarcode, listSeq in dictBarcodeSeq.items():
+        with open('../Base_edit_2/Input/{user}/Query/{project}/{sample}/{barcode}.txt'.format(
+                user=strUser, project=strProject, sample=strSample, barcode=strBarcode), 'w') as Output:
+            Output.write(''.join(listSeq))
 
-                elif intCheckTotLine == 1:
-                    dictBarcodeSeq[strBarcode].append(strRow)
-                    intOneLineMore = 1
-
-            for strBarcode, listSeq in dictBarcodeSeq.items():
-                with open('../Base_edit_2/Input/{user}/Query/{project}/{sample}/{barcode}.txt'.format(
-                        user=strUser, project=strProject, sample=strSample, barcode=strBarcode), 'w') as Output:
-                    Output.write(''.join(listSeq))
-
-            Total_result_file.close()
+    Total_result_file.close()
 
 
 def Main():
     print('Program Start')
-    Convert_Indelsearcher_output()
+    p = mp.Pool(4)
+
+    with open('./User/{user}/{project}.txt'.format(user=strUser, project=strProject)) as SampleList:
+        listSampleRefGroup = [strSampleRefGroup for strSampleRefGroup in SampleList if strSampleRefGroup[0] != '#']
+        p.map_async(Convert_Indelsearcher_output, listSampleRefGroup).get()
+
+    p.close()
+
     print('Program End')
+
 
 Main()
