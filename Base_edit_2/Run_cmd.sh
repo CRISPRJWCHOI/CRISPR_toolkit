8326760ab2b4724d30f0498262f65f62128b4b96
CRISPRJWCHOI <jwchoi501abmrc@gmail.com>
2019-10-10 15:20:49 +0900

    update

--- Base_edit_2/Run_cmd.sh
+++ Base_edit_2/Run_cmd.sh
@@ -28,6 +28,6 @@ done < ../PythonPath.txt
 [ ! -d ./Output/${user}/${project} ] && { `mkdir ./Output/${user}/${project}`; }
 [ ! -d ./Output/${user}/${project}/Log ] && { `mkdir ./Output/${user}/${project}/Log`; }
 
-$python ./Run_BaseEdit_freq.py --python $python --user $user --project $project -w $target_window --indel_check_pos $indel_check_pos \
+nohup $python ./Run_BaseEdit_freq.py --python $python --user $user --project $project -w $target_window --indel_check_pos $indel_check_pos \
                                --target_ref_alt $target_ref_alt --PAM_seq $PAM_seq --PAM_pos $PAM_pos --Guide_pos $Guide_pos \
                                --gap_open $gap_open --gap_extend $gap_extend -t $thread > ./Output/${user}/${project}/Log/log.txt 2>&1 &
